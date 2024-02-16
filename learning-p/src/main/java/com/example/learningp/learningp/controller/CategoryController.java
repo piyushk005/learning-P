501dbc7ae8b38caeb2b19396de8fafba4bf4b367
piyushk005 <158276069+piyushk005@users.noreply.github.com>
2024-02-16 13:03:38 +0530

    Update CategoryController.java

--- learning-p/src/main/java/com/example/learningp/learningp/controller/CategoryController.java
+++ learning-p/src/main/java/com/example/learningp/learningp/controller/CategoryController.java
@@ -2,7 +2,11 @@ package com.example.learningp.learningp.controller;
 
 
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.learningp.learningp.Dto.CategoryDto;
+import com.example.learningp.learningp.entity.Category;
 import com.example.learningp.learningp.service.CategoryService;
 
 import jakarta.validation.Valid;
@@ -19,16 +24,26 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
-   @Autowired
-   private CategoryService categoryService;
-   
-   @PostMapping("/create")
-   public ResponseEntity<?> createCategory(@Valid @RequestBody CategoryDto categoryDto){
-	   return categoryService.addCategory(categoryDto);
-   }
-   
-   @GetMapping("/all")
-   public ResponseEntity<?> getAllCategory(){
-	   return categoryService.getCategory();
-   }
+	@Autowired
+	private CategoryService categoryService;
+
+	@PostMapping("/create")
+	public ResponseEntity<?> createCategory(@Valid @RequestBody CategoryDto categoryDto) {
+		boolean add = categoryService.addCategory(categoryDto);
+		HashMap<String, String> response = new HashMap<>();
+
+		if (add) {
+			response.put("Message", "Category Added.");
+			return new ResponseEntity<>(response, HttpStatus.CREATED);
+		} else {
+			response.put("Message", "Something went wrong");
+			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@GetMapping("/all")
+	public ResponseEntity<?> getAllCategory() {
+		HashMap<String, List<Category>> response = new HashMap<>();
+		return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+	}
 }
