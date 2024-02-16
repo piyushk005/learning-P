77d606a44255e6d53fb0413b7adb429b1e3bf24d
piyushk005 <158276069+piyushk005@users.noreply.github.com>
2024-02-16 13:02:50 +0530

    Update AdminController.java

--- learning-p/src/main/java/com/example/learningp/learningp/controller/AdminController.java
+++ learning-p/src/main/java/com/example/learningp/learningp/controller/AdminController.java
@@ -1,6 +1,9 @@
 package com.example.learningp.learningp.controller;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,17 +19,34 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
-     @Autowired
-     private UserService userService;
-     
-     @PostMapping("/register")
-     public ResponseEntity<?> register(@Valid @RequestBody RegisterUserDto  registerUserDto){
-    	 return userService.registerUser(registerUserDto);
-     }
-     
-     @PostMapping("/login")
-     public ResponseEntity<?> login(@Valid @RequestBody LoginUserDto loginUserDto){
-    	 return userService.loginUser(loginUserDto);
-     }
-}
+	@Autowired
+	private UserService userService;
 
+	@PostMapping("/register")
+	public ResponseEntity<?> register(@Valid @RequestBody RegisterUserDto registerUserDto) {
+		boolean register = userService.registerUser(registerUserDto);
+		HashMap<String, String> response = new HashMap<>();
+
+		if (register) {
+			response.put("Message", "User registered successfully.");
+			return new ResponseEntity<>(response, HttpStatus.CREATED);
+		} else {
+			response.put("Error", "User already exists");
+			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@Valid @RequestBody LoginUserDto loginUserDto) {
+		boolean login = userService.loginUser(loginUserDto);
+		HashMap<String, String> response = new HashMap<>();
+
+		if (login) {
+			response.put("Message", "User logged in Successfully");
+			return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+		} else {
+			response.put("Message", "User authentication failed");
+			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+		}
+	}
+}
